3430c255fa46c039a65f0c75a0dbb3765cfdb4f1
Matheus Soares Bortolin <matbort@fcm.unicamp.br>
2023-10-02 15:09:20 -0300

    primeiro commit

---
+++ src/locadora/model/Ator.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package locadora.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author matbort
+ */
+public class Ator {
+    
+    private Integer codAtor;
+    private String name;
+    private String nacionalidade;
+    private ArrayList<Filme> filmes = new ArrayList<Filme>();
+
+    
+}
